e50bf1c53bcdab8d9fd839b794608e4c28706ed1
opilane <opilane@HP-CZC2349HT9>
2025-01-27 10:53:34 +0200

    Add project files.

---
+++ Lihtsad_programmid.py
@@ -0,0 +1,28 @@
+# from calendar import *
+# from datetime import *
+# tana = date.today()
+# print(f"Tere tana on: {tana}")
+
+# #27/12/2025
+# tana1 = tana.strftime("%d/%m/%Y")
+# print(f"Tere, tana on {tana1}")
+
+# #jaanuar 27, 2025
+# tana1 = tana.strftime("%B %d, %Y")
+# print(f"Tere, tana on {tana1}")
+
+# # 12/27/22
+# tana1 = tana.strftime("%m/%d/%y")
+# print(f"Tere, tana on {tana1}")
+
+# # Dec-27-2022
+# tana1 = tana.strftime("%b-%d-%Y")
+# print(f"Tere, tana on {tana1}")
+# paevadekogus=monthrange(2025,1)
+# print(paevadekogus)
+# print(f"Jaanuaris on {paevadekogus} paeva")
+# paevad=tana.day
+# onjaanud=paevadekogus-paevad
+# print(f"Jaanuaris on jaanud {onjaanud} paeva")
+# aastalopuni=365-monthrange(2025,1)[1]+onjaanud
+# print(f"Assta lopuni on jaanud {aastalopuni} paeva")
---
+++ module1.py
@@ -0,0 +1,26 @@
+﻿#task 2
+
+from math import *
+a = 3 + 8 / (4 - 2) * 4 #esimene variant
+b = 3 + 8 / 4 - (2 * 4) #sin on teine variant, esimene on verang 2 * 4 ja pärast kõik 
+c = 3 + (8 / 4) - 2 * 4 #
+d = (3 + 8) / 4 - 2 * 4
+f = (3 + 8 / 4) - 2 * 4
+print(a, b, c, d, f)
+
+#task 3
+#Внутри квадрата находится круг. Радиус круга равен R.
+#Найдите и выведите на экран площадь квадрата, периметр квадрата, площадь круга и длину окружности круга.
+from math import *
+try:   
+    r=float(input("Mis on raadius?: "))
+    Sr=round((2*r)**2,2)
+    So=round(pi*r**2,2)
+    Pr=round(8*r,2)
+    Prin=round(2*pi*r,2)
+    print(f"vastus: Ruudu pindala: {Sr}, nringi pindala on: {So}, ruudu umberemott: {Pr}, Ringi umbermoot on: {Prin}. ")
+    print()
+except:
+    print("Sisesta ujukomaarv!")
+
+
